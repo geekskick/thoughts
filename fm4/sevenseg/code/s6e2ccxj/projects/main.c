683d02b7163ec3f6c2f20b77b3ade036d4b3a61a
Pat <geeks_kick@yahoo.co.uk>
2017-08-12 22:14:20 +0100

    Sevensegment code uses timer interrupt to change between 1 and 2.

--- fm4/sevenseg/code/s6e2ccxj/projects/main.c
+++ fm4/sevenseg/code/s6e2ccxj/projects/main.c
@@ -11,11 +11,59 @@
 */
 #include "mcu.h"
 #include "gpio.h"
+#include "dt.h"
 
-int main(void)
-{
- 
-	// All segments on
+#define COUNTS_FOR_1_S                                                156250u
+
+void show_one( void );
+void show_two( void );
+void show_three( void );
+void show_four( void );
+void show_five( void );
+void show_six( void );
+void show_seven( void );
+void show_eight( void );
+void show_nine( void );
+void show_zero( void ); 
+
+static uint32_t timer_count;
+
+static void DtCallback(void){
+    Dt_ClrIrqFlag(DtChannel0);    /* Clear Irq */
+		switch(timer_count){
+			case 0: show_one(); timer_count++;   break;
+			case 1: show_two(); timer_count = 0; break;
+			default: timer_count = 0; break;
+		}
+		
+}
+
+void start_timer( const uint32_t value ){
+ /* Write load value for channel  */
+  Dt_WriteLoadVal(value, DtChannel0);
+  /* Start count for channel 0 */
+  Dt_EnableCount(DtChannel0);
+}
+
+void configure_timer( void ){
+	stc_dt_channel_config_t stcDtChannelConfig0;
+  
+	timer_count = 0;
+  
+  /* Initialize configuration */
+  stcDtChannelConfig0.u8Mode = DtPeriodic;
+  stcDtChannelConfig0.u8PrescalerDiv = DtPrescalerDiv256;
+  stcDtChannelConfig0.u8CounterSize = DtCounterSize32; 
+  stcDtChannelConfig0.bIrqEnable = TRUE;
+  stcDtChannelConfig0.pfnIrqCallback = DtCallback;
+  stcDtChannelConfig0.bTouchNvic = TRUE;
+	Dt_WriteBgLoadVal(COUNTS_FOR_1_S, DtChannel0);
+	Dt_WriteLoadVal(COUNTS_FOR_1_S, DtChannel0);
+	Dt_Init( &stcDtChannelConfig0, DtChannel0 );
+}
+
+void configure_gpio( void ){
+		// All segments on
   Gpio1pin_InitOut(GPIO1PIN_P16, Gpio1pin_InitVal(1u));
 	Gpio1pin_InitOut(GPIO1PIN_P42, Gpio1pin_InitVal(1u));
 	Gpio1pin_InitOut(GPIO1PIN_P40, Gpio1pin_InitVal(1u));
@@ -23,11 +71,39 @@ int main(void)
 	Gpio1pin_InitOut(GPIO1PIN_PF0, Gpio1pin_InitVal(1u));
 	Gpio1pin_InitOut(GPIO1PIN_PF1, Gpio1pin_InitVal(1u));
 	Gpio1pin_InitOut(GPIO1PIN_P7D, Gpio1pin_InitVal(1u));
-	
-    
+}
+
+int main(void)
+{
+  configure_gpio();
+	configure_timer();
+	start_timer( COUNTS_FOR_1_S );
+	  
     while(1)
     {
+
     }
 }
+
+void show_one( void ){
+	Gpio1pin_Put(GPIO1PIN_P16,0u );
+	Gpio1pin_Put(GPIO1PIN_P42,1u );
+	Gpio1pin_Put(GPIO1PIN_P40,1u );
+	Gpio1pin_Put(GPIO1PIN_P41,0u );
+	Gpio1pin_Put(GPIO1PIN_PF0,0u );
+	Gpio1pin_Put(GPIO1PIN_PF1,0u );
+	Gpio1pin_Put(GPIO1PIN_P7D,0u );
+}
+
+void show_two( void ){
+	Gpio1pin_Put( GPIO1PIN_P16, 1u );
+	Gpio1pin_Put( GPIO1PIN_P42, 1u );
+	Gpio1pin_Put( GPIO1PIN_P40, 0u );
+	Gpio1pin_Put( GPIO1PIN_P41, 1u );
+	Gpio1pin_Put( GPIO1PIN_PF0, 1u );
+	Gpio1pin_Put( GPIO1PIN_PF1, 0u );
+	Gpio1pin_Put( GPIO1PIN_P7D, 1u );
+}
+
 
 /* [] END OF FILE */
--- fm4/sevenseg/index.md
+++ fm4/sevenseg/index.md
@@ -32,3 +32,16 @@ int main(void)
 ```
 And my output:
 <img src="all_on.jpg">
+
+#Controlling It
+With the 7 segement display I want to make somethings appear, `1`, `2`, `3`, ... and so on. And to make a segment turn on, using my __common cathode__ display I need to make the input to it `HIGH`. To make a 1 appear I need to have segments `b` and `c` on, and the rest off, for example. I made a table to help me with this:
+
+| Segment | Pin | Port/Pin | Display `1` | Display `2` | Display `3` |Display `4` |Display `5` |Display `6` |Display `7` |Display `8` |Display `9` |Display `0` |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| a | 55| 7D | LOW  | HIGH |HIGH |LOW |  HIGH |HIGH |HIGH |HIGH |HIGH |HIGH |
+| b | 64| F1 | HIGH | HIGH |HIGH |HIGH | LOW  |LOW  |HIGH |HIGH |HIGH |HIGH |
+| c | 63| F0 | HIGH | LOW  |HIGH |HIGH | HIGH |HIGH |HIGH |HIGH |HIGH |HIGH |
+| d | 47| 41 |  LOW |  HIGH| HIGH| LOW|   HIGH| HIGH| LOW |HIGH |HIGH |HIGH |
+| e | 46| 40 |  LOW |  HIGH| LOW | LOW |  LOW | HIGH| LOW |HIGH |LOW  |HIGH |
+| f | 42| 42 | LOW  | LOW  |LOW  |HIGH  | HIGH| HIGH|LOW  |HIGH |HIGH |HIGH |
+| g | 100| 16| LOW  | HIGH |HIGH |HIGH | HIGH |HIGH |LOW  |HIGH |HIGH |LOW  |
